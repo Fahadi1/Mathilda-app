3248a85239aab997c616a5c960542e1dd552ebeb
Fahadi1 <73292833+Fahadi1@users.noreply.github.com>
2021-07-28 15:38:07 +0200

    Update MathildaApplication.java

--- src/main/java/fr/eql/al35/MathildaApplication.java
+++ src/main/java/fr/eql/al35/MathildaApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.event.EventListener;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MathildaApplication {
 	
 	static Logger log = LoggerFactory.getLogger(MathildaApplication.class);
